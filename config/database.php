42c62e86a391ad643cf316364c7f9b95d358e3b7
RUHAN <146383502+anikruhan7@users.noreply.github.com>
2026-05-18 17:02:02 +0600

    Fix merge conflict in database.php

--- config/database.php
+++ config/database.php
@@ -1,20 +1,16 @@
 <?php
 class Database
 {
-    private $host = 'localhost';
-    private $user = 'root';
-    private $pass = '';
-    private $dbname = 'quizplatform';
-    private $conn;
+        private $host = 'localhost';
+        private $user = 'root';
+        private $pass = '';
+        private $dbname = 'quizplatform';
+        private $conn;
 
-    public function getConnection()
-    {
-        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
-<<<<<<< HEAD
-        if ($this->conn->connect_error) die("Connection failed: " . $this->conn->connect_error);
-=======
-        if ($this->conn->connect_error) die("DB connection failed: " . $this->conn->connect_error);
->>>>>>> 22feb9917b75bd316a893fbaa97bfdb8d2e49a84
-        return $this->conn;
-    }
+        public function getConnection()
+        {
+                $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
+                if ($this->conn->connect_error) die("Connection failed: " . $this->conn->connect_error);
+                return $this->conn;
+        }
 }
